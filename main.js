10968c403a5534f1e2020f3553ed0ad786192e36
José Mendoza <joseantoniomendozanavarro@gmail.com>
2024-02-05 16:16:59 -0600

    added coordinates scrap and also implemented departments scrapper

--- main.js
+++ main.js
@@ -1,9 +1,14 @@
-import {Scrapper, SellingHousesJal} from './scrapper.js';
+import {SellingHousesJal, SellingDepartmentsJalisco} from './scrapper.js';
 
 function sellingHousesJal() {
-    
     const scrapper = new SellingHousesJal('https://www.inmuebles24.com/casas-en-venta-en-jalisco.html');
     scrapper.scrapeUrls(2,4,3000);
 }
 
+function sellingDepartmentsJalisco() {
+    const scrapper = new SellingDepartmentsJalisco('https://www.inmuebles24.com/departamentos-en-venta-en-jalisco.html');
+    scrapper.scrapeUrls(2,4,3000);
+}
+
 sellingHousesJal();
+sellingDepartmentsJalisco();
--- scrapper.js
+++ scrapper.js
@@ -4,6 +4,7 @@ import fs from 'fs';
 
 puppeteer.use(StealthPlugin());
 
+
 class Scrapper {
   constructor(url) {
     this._url = url;
@@ -12,6 +13,7 @@ class Scrapper {
 
 
   async init() {
+    console.log('Initializing browser');
     this._browser = await puppeteer.launch({
       headless: true,
         protocolTimeout: 30000, 
@@ -20,7 +22,7 @@ class Scrapper {
     this._browser.on('targetcreated', async target => {
         const page = await target.page();
         if (page) {
-            page.setDefaultTimeout(30000); 
+            page.setDefaultTimeout(300000); 
         }
     });
 }
@@ -46,6 +48,40 @@ class Scrapper {
     }
   }
 
+
+  async getCoordinates(url) {
+    if(!this._browser) {
+      await this.init();
+    }
+    if (!this._page) {
+      this._page = await this._browser.newPage();
+  }
+    try {
+      await this._page.goto(url, { waitUntil: 'domcontentloaded', timeout: 0 });
+      await this._page.waitForSelector('.static-map');
+      const data = await this._page.evaluate(() => {
+        const mapElement = document.querySelector('.static-map');
+        const mapUrl = mapElement.getAttribute('src');
+        const coordinates = {};
+        if (mapUrl) {
+          const coordinatesString =  mapUrl.split('center=')[1];
+          if (coordinatesString) {
+            let [latitude, longitude] = coordinatesString.split(',');
+            longitude = longitude.split('&')[0];
+            coordinates.latitude = latitude.trim();
+            coordinates.longitude = longitude.trim();
+          }
+        }
+        return coordinates;
+      });
+      return data;
+    } catch (error) {
+      console.error('Error getting coordinates:', error);
+    } 
+  }
+  
+
+
   async scrap() {
     if(!this._browser) {
       await this.init();
@@ -55,53 +91,73 @@ class Scrapper {
     try {
         await page.goto(this._url, { waitUntil: 'domcontentloaded', timeout: 0 });
 
-        await this.handleCookies(page);
+        //await this.handleCookies(page);
 
-        await page.waitForFunction(() => document.querySelector('.sc-i1odl-2 sc-i1odl-0'));
+        await page.waitForFunction(() => document.querySelector('.sc-i1odl-0'));
 
         const data = await page.evaluate(() => {
             const result = {};
-            const estate = document.querySelectorAll('.sc-i1odl-0');
-            
-            estate.forEach((estate, estateId) => {
-                const locationElement = estate.querySelector('[data-qa="POSTING_CARD_LOCATION"]');
-                const price = estate.querySelector('[data-qa="POSTING_CARD_PRICE"]');
-                const priceFromElement = estate.querySelector('[data-qa="POSTING_CARD_PRICE_FROM"]');
-                const priceToElement = estate.querySelector('[data-qa="POSTING_CARD_PRICE_TO"]');
-                const description = estate.querySelector('[data-qa="POSTING_CARD_DESCRIPTION"]');
-                const featuresElement = estate.querySelector('[data-qa="POSTING_CARD_FEATURES"]');
+            const estateList = document.querySelectorAll('.sc-i1odl-0');
 
-                const estateData = {};
+            for (const [estateId, estate] of Object.entries(estateList)) {
 
-                if(locationElement?.innerText.trim()) {
-                    estateData.location = locationElement?.innerText.trim();
-                }
-                if(price?.innerText.trim()) {
-                    estateData.price = price?.innerText.trim();
-                }
-                if(priceFromElement?.innerText.trim()) {
-                    estateData.priceFrom = priceFromElement?.innerText.trim();
-                }
-                if(priceToElement?.innerText.trim()) {
-                    estateData.priceTo = priceToElement?.innerText.trim();
-                }
+              const locationElement = estate.querySelector('[data-qa="POSTING_CARD_LOCATION"]');
+              const price = estate.querySelector('[data-qa="POSTING_CARD_PRICE"]');
+              const priceFromElement = estate.querySelector('[data-qa="POSTING_CARD_PRICE_FROM"]');
+              const priceToElement = estate.querySelector('[data-qa="POSTING_CARD_PRICE_TO"]');
+              const description = estate.querySelector('[data-qa="POSTING_CARD_DESCRIPTION"]');
+              const featuresElement = estate.querySelector('[data-qa="POSTING_CARD_FEATURES"]');
 
-                if(description?.innerText.trim()) {
-                    estateData.description = description?.innerText.trim();
-                }
+              const estateData = {};
 
-                if(featuresElement?.innerText.trim()) {
-                    estateData.features = featuresElement?.innerText.trim();
-                }
+              if(locationElement?.innerText.trim()) {
+                  estateData.location = locationElement?.innerText.trim();
+              }
+              if(price?.innerText.trim()) {
+                  estateData.price = price?.innerText.trim();
+              }
+              if(priceFromElement?.innerText.trim()) {
+                  estateData.priceFrom = priceFromElement?.innerText.trim();
+              }
+              if(priceToElement?.innerText.trim()) {
+                  estateData.priceTo = priceToElement?.innerText.trim();
+              }
 
-                result[estateId + 1] = estateData;
-            });
+              if(description?.innerText.trim()) {
+                  estateData.description = description?.innerText.trim();
+              }
 
-            return result;
+              if(featuresElement?.innerText.trim()) {
+                  estateData.features = featuresElement?.innerText.trim();
+              }
+
+              const postingType = estate.getAttribute('data-posting-type');
+              if (postingType === 'DEVELOPMENT'){
+                dataToPostingValue = estate.getAttribute('data-to-posting');
+                estateData.stage = 'development'
+              }
+              
+              if (postingType === 'PROPERTY'){
+                dataToPostingValue = estate.getAttribute('data-to-posting');
+                estateData.stage = 'finished'
+              }
+
+              if (dataToPostingValue){
+                estateData.url = 'https://www.inmuebles24.com'+dataToPostingValue;
+              }
+              result[estateId + 1] = estateData;
+
+            }
+
+
+          return result;
         });
         return data;
     } catch (error) {
         console.error('Error during execution:', error);
+    } finally {
+        await page.close();
+        //await this._browser.close();
     }
 }
 
@@ -110,23 +166,42 @@ class Scrapper {
 class SellingHousesJal extends Scrapper {
     constructor(url) {
         super(url);
+        this._page = null;
     }
 
     async scrapeUrls(startPage, endPage, delay) {
       const allData = {};
       const firstPage = await this.scrap(); 
+      for (const [key, property] of Object.entries(firstPage)) {
+        if (property.url) {
+          const coordinates = await this.getCoordinates(property.url);
+          property.coordinates = coordinates;
+        }
+      }
       allData[this.url] = firstPage;
       for (let i = startPage; i <= endPage; i++) {
         const url = `https://www.inmuebles24.com/casas-en-venta-en-jalisco-pagina-${i}.html`;
         this.url = url; 
   
         const data = await this.scrap(); 
+        for (const [key, property] of Object.entries(data)) {
+          if (property.url) {
+            const coordinates = await this.getCoordinates(property.url);
+            property.coordinates = coordinates;
+          }
+        }
         allData[url] = data;
   
         await new Promise(resolve => setTimeout(resolve, delay));
       }
+
+      const fileName = 'data.json';
+
+      if(fs.existsSync(fileName)){
+        fs.unlinkSync(fileName);
+      }
   
-      fs.writeFile('data.json', JSON.stringify(allData), (err) => {
+      fs.writeFile('dataHouses.json', JSON.stringify(allData), (err) => {
         if (err) throw err;
         console.log('Data written to file');
     });
@@ -134,4 +209,49 @@ class SellingHousesJal extends Scrapper {
 }
 
 
-export {SellingHousesJal, Scrapper}
+class SellingDepartmentsJalisco extends Scrapper {
+  constructor(url) {
+    super(url);
+    this._page = null;
+  }
+  async scrapeUrls(startPage, endPage, delay) {
+    const allData = {};
+    const firstPage = await this.scrap(); 
+    for (const [key, property] of Object.entries(firstPage)) {
+      if (property.url) {
+        const coordinates = await this.getCoordinates(property.url);
+        property.coordinates = coordinates;
+      }
+    }
+    allData[this.url] = firstPage;
+    for (let i = startPage; i <= endPage; i++) {
+      const url = `https://www.inmuebles24.com/departamentos-en-venta-en-jalisco-pagina-${i}.html`;
+      this.url = url; 
+
+      const data = await this.scrap(); 
+      for (const [key, property] of Object.entries(data)) {
+        if (property.url) {
+          const coordinates = await this.getCoordinates(property.url);
+          property.coordinates = coordinates;
+        }
+      }
+      allData[url] = data;
+
+      await new Promise(resolve => setTimeout(resolve, delay));
+    }
+
+    const fileName = 'data.json';
+
+    if(fs.existsSync(fileName)){
+      fs.unlinkSync(fileName);
+    }
+
+    fs.writeFile('dataDepartments.json', JSON.stringify(allData), (err) => {
+      if (err) throw err;
+      console.log('Data written to file');
+    });
+  }
+}
+
+
+export {SellingHousesJal, SellingDepartmentsJalisco}
